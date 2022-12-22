77f3044c558f9d6c1ac5728976907c03715dee9a
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-12-21 18:20:28 -0700

    commit tree 3f19c1c1c496ea857312958e10dc1aa2ca3476c6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+ const users = {
+        name : name,
+        email : email
+ };
+ 
+ const configurationUser = {
+    method : "POST",
+    headers :{
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+    },
+    body : JSON.stringify(users)
+    };
+ 
+    return fetch("http://localhost:3000/users", configurationUser)
+    .then (response => response.json())
+    .then (users => {
+        document.body.innerHTML = users.id 
+    })
+    .catch (function(error){
+     alert("Ca marche pas, essaye encore !");
+     document.body.innerHTML = error.message
+    })
+}
+
+submitData("John", "jphn@hhkl.com")
